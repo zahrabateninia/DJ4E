81593b8dc38617fb4407294d458f96dee3e09e24
zahrabateninia <zahrabatenin@gmail.com>
2025-02-07 08:54:19 +0330

    Use FormHelper to add action and form method for the form

--- django_crispy_forms/university_form/forms.py
+++ django_crispy_forms/university_form/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from datetime import datetime
+from django.urls import reverse_lazy
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 
 class UniversityForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_action = reverse_lazy('index')
+        self.helper.form_method = 'GET'
+
     SUBJECT_CHOICES = (
         (1, 'Web development'),
         (2, 'Systems programming'),
