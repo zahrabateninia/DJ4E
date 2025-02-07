f24a69199b81e7e9a2b2d8bc85cf4db73520d7f0
zahrabateninia <zahrabatenin@gmail.com>
2025-02-07 08:06:11 +0330

    Load crispy forms and use it in base.html template

--- django_crispy_forms/django_crispy_forms/settings.py
+++ django_crispy_forms/django_crispy_forms/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms'
+    'crispy_forms',
+    'crispy_bootstrap4',
     'university_form',
 ]
 
